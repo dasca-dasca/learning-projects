fc3da9dc2b5b876005baa602ab8ba43d216df577
Andrei D <andrei.dascaliuc@pm.me>
2024-02-04 07:47:12 +0200

    Added new card images; Added buttons logic; Added double functionality;

--- 04-blackjack/scripts/app.js
+++ 04-blackjack/scripts/app.js
@@ -4,24 +4,37 @@ import { Deck } from './card-and-deck.js';
 export class Game {
     constructor() {
         this.deck = new Deck();
-        this.human = new Human(1000);
+        this.human = new Human(200);
         this.computer = new Computer();
         this.setupEvents();
     }
 
+    //Buttons
+    buttons = {
+        betBtn: document.getElementById('bet-btn'),
+        hitBtn: document.getElementById('hit-btn'),
+        doubleBtn: document.getElementById('double-btn'),
+        splitBtn: document.getElementById('split-btn'),
+        stayBtn: document.getElementById('stay-btn'),
+    };
+
     setupEvents() {
-        document
-            .getElementById('bet-btn')
-            .addEventListener('click', this.human.bet.bind(this, this.human));
-        document
-            .getElementById('hit-btn')
-            .addEventListener(
-                'click',
-                this.human.hit.bind(this, this.human, this.deck)
-            );
-        // document
-        //     .getElementById('stay-btn')
-        //     .addEventListener('click', this.stay.bind(this));
+        this.buttons.betBtn.addEventListener(
+            'click',
+            this.human.bet.bind(this)
+        );
+        this.buttons.hitBtn.addEventListener(
+            'click',
+            this.human.hit.bind(this)
+        );
+        this.buttons.stayBtn.addEventListener(
+            'click',
+            this.computer.stay.bind(this)
+        );
+        this.buttons.doubleBtn.addEventListener(
+            'click',
+            this.human.double.bind(this)
+        );
     }
 
     static updateBalance(amount) {
@@ -29,10 +42,15 @@ export class Game {
     }
 
     deal() {
+        //Deal initial cards
         this.showCard(this.human.draw(this.deck), true, false, 0);
         this.showCard(this.computer.draw(this.deck), true, true, 0);
         this.showCard(this.human.draw(this.deck), true, false, 1);
         this.showCard(this.computer.draw(this.deck), false, true, 1); // Don't show the second dealer card.
+
+        //Disable bet btn
+        this.manageButtons([0, 1, 1, 1, 1])
+
         // this.checkForBlackjack();
 
         // this.dealerAmount = this.dealerLogic(true);
@@ -48,17 +66,106 @@ export class Game {
             newImg.classList.add('dealer-card');
             newImg.style.top = `${position * 30}px`;
             newImg.style.right = `${position * 30}px`;
+            this.showAmount(dealerPosition, this.computer, true);
         } else {
             newImg.classList.add('player-card');
             newImg.style.bottom = `${position * 30}px`;
             newImg.style.left = `${position * 30}px`;
             playerPosition.appendChild(newImg);
+            this.showAmount(playerPosition, this.human);
         }
         if (show) {
             newImg.src = card.path;
         } else {
             newImg.src = `/images/card-back.png`;
+            newImg.id = 'face-down-card';
         }
+        if (show && dealer) {
+            this.showAmount(dealerPosition, this.computer);
+        }
+    }
+
+    showAmount(position, player, hide = false) {
+        const where = position.firstElementChild;
+        where.style.position = 'absolute';
+        if (player === this.computer) {
+            where.style.left = '70px';
+        } else {
+            where.style.right = '70px';
+        }
+
+        if (hide === true) {
+            where.textContent = player.cards[0].amount;
+            return;
+        }
+
+        player.amount = player.calculateTotal();
+        where.textContent = player.amount;
+    }
+
+    checkResult() {
+        if (this.human.amount === this.computer.amount) {
+            console.log(
+                `Push! Dealer ${this.computer.amount} - ${this.human.amount} Player`
+            );
+            this.human.balance += this.human.currentBet;
+        } else if (
+            this.human.amount < this.computer.amount &&
+            this.computer.amount < 22
+        ) {
+            console.log(
+                `Dealer Wins! Dealer ${this.computer.amount} - ${this.human.amount} Player`
+            );
+        } else if (
+            this.human.amount > this.computer.amount ||
+            this.computer.amount > 21
+        ) {
+            console.log(
+                `Player Wins! Player ${this.human.amount} - ${this.computer.amount} Dealer`
+            );
+            this.human.balance += this.human.currentBet * 2;
+        }
+        this.manageButtons([1, 0, 0, 0, 0])
+        Game.updateBalance(this.human.balance);
+    }
+
+    win() {}
+
+    lose() {
+    }
+
+
+    manageButtons(binaryArray) {
+        //The array should contain 1-5 binary digits
+        //Each 1 represents a button on
+        //Each 0 represents a button off
+        if (binaryArray.length !== 5) {
+            console.error(
+                'Invalid binary array. It must have exactly 5 elements.'
+            );
+            return;
+        }
+
+        Object.keys(this.buttons).forEach((buttonKey, index) => {
+            const button = this.buttons[buttonKey];
+            const isEnabled = binaryArray[index] === 1;
+            button.disabled = !isEnabled;
+        });
+    }
+
+    resetRound() {
+        this.deck = new Deck();
+
+        for (const value of [this.human, this.computer]) {
+            value.cards = [];
+            value.amount = 0;
+        }
+        this.human.currentBet = 0;
+
+        document.querySelectorAll('.card').forEach((card) => card.remove());
+        document.querySelectorAll('.has').forEach((value) => {
+            value.textContent = '';
+        });
     }
 }
 
--- 04-blackjack/scripts/player.js
+++ 04-blackjack/scripts/player.js
@@ -1,5 +1,4 @@
-import { Deck } from './card-and-deck.js';
-import { Game } from './app.js'
+import { Game } from './app.js';
 
 class Player {
     constructor() {
@@ -17,12 +16,12 @@ class Player {
         let total = 0;
         let ace = 0;
         for (const card of this.cards) {
-            if (card.amount === 11){
+            if (card.amount === 11) {
                 ace += 1;
             }
             total += card.amount;
         }
-        if (total > 21 && ace){
+        if (total > 21 && ace > 0) {
             total -= ace * 10;
         }
         return total;
@@ -36,220 +35,86 @@ export class Human extends Player {
         this.currentBet = 0;
     }
 
-    hit(human, deck) {
-        this.showCard(human.draw(deck), true, false, human.cards.length - 1);
-        human.amount = human.playerLogic();
-    }
-
-    playerLogic() {
-        let total = this.calculateTotal();
-        if (total > 21) {
-            console.log(`* Bust!`);
-            //LOSE
-        } else if(total < 21){
-
-        }else{
-            if (total === 21) {
-                //STAY FORCED
-            }
-        }
-        console.log(`* PLAYER has ${total}.`);
-        return total;
-    }
-
-    bet(human) {
+    bet() {
+        this.resetRound();
         const amount = Number.parseInt(
             document.getElementById('bet-amount').value
         );
-        if (amount > human.balance) {
-            console.log(`Not enough funds. Balance: $${human.balance}`);
+        if (amount > this.human.balance) {
+            console.log(`Not enough funds. Balance: $${this.human.balance}`);
             return;
         }
-        human.balance -= amount;
-        human.currentBet = amount;
-        Game.updateBalance(human.balance);
+        this.human.balance -= amount;
+        this.human.currentBet = amount;
+        Game.updateBalance(this.human.balance);
         this.deal();
+    }
+
+    hit() {
+        this.manageButtons([0, 1, 0, 0, 1]);
+        this.showCard(
+            this.human.draw(this.deck),
+            true,
+            false,
+            this.human.cards.length - 1
+        );
+        this.human.amount = this.human.calculateTotal();
+
+        if (this.human.amount > 21) {
+            this.lose();
+        }
+    }
+    double() {
+        if (this.human.balance < this.human.currentBet) {
+            console.log('Not enough balance to double.');
+            return;
+        }
+        this.human.balance -= this.human.currentBet;
+        Game.updateBalance(this.human.balance);
+        this.human.currentBet *= 2;
+        this.showCard(
+            this.human.draw(this.deck),
+            true,
+            false,
+            this.human.cards.length - 1
+        );
+        this.human.amount = this.human.calculateTotal();
+        this.manageButtons([1, 0, 0, 0, 0]);
+
+        if (this.human.amount > 21) {
+            this.lose();
+            return;
+        }
+        this.computer.stay.call(this);
     }
 }
 export class Computer extends Player {
     constructor() {
         super();
     }
+
+    stay() {
+        //Remove face down card
+        const faceDownCard = document.getElementById('face-down-card');
+        faceDownCard.remove();
+
+        //Show what was the face down card
+        this.showCard(
+            this.computer.cards[1],
+            true,
+            true,
+            this.computer.cards.length - 1
+        );
+
+        //Logic
+        this.computer.amount = this.computer.calculateTotal();
+
+        while (this.computer.amount < 17) {
+            const nextCard = this.computer.draw(this.deck);
+            this.showCard(nextCard, true, true, this.computer.cards.length - 1);
+            this.computer.amount = this.computer.calculateTotal();
+        }
+
+        this.checkResult();
+    }
 }
-
-//TO ADD TO A SHOW METHOD
-// if (show) {
-//     console.log(
-//         `${playerOrDealer.toUpperCase()} draws ${nextCard.value} of ${
-//             nextCard.type
-//         }.`
-//     );
-// } else {
-//     console.log(`${playerOrDealer.toUpperCase()} draws ???.`);
-// }
-
-// class Game {
-//     constructor(balance) {
-//         this.balance = balance;
-//         this.deck = new Deck();
-//         this.currentBet = 0;
-//         // this.playerCards = [];
-//         // this.playerAmount = 0;
-//         // this.dealerCards = [];
-//         // this.dealerAmount = 0;
-//         this.setupEvents();
-//     }
-
-//     setupEvents() {
-//         document
-//             .getElementById('bet-btn')
-//             .addEventListener('click', this.bet.bind(this));
-//         document
-//             .getElementById('hit-btn')
-//             .addEventListener('click', this.hit.bind(this));
-//         document
-//             .getElementById('stay-btn')
-//             .addEventListener('click', this.stay.bind(this));
-//     }
-
-//     doubleBtn = document.getElementById('double-btn');
-//     splitBtn = document.getElementById('split-btn');
-
-//     bet() {
-//         const amount = Number.parseInt(
-//             document.getElementById('bet-amount').value
-//         );
-//         if (amount > this.balance) {
-//             console.log(`Not enough funds. Balance: $${this.balance}`);
-//             return;
-//         }
-//         this.balance -= amount;
-//         this.currentBet = amount;
-//         this.updateBalance(this.balance);
-//         this.deal();
-//     }
-
-//     updateBalance(amount) {
-//         document.getElementById('balance').textContent = `$${amount}`;
-//     }
-
-//     // draw(playerOrDealer, show = true) {
-//     //     const nextCard = this.deck.cards.shift();
-
-//     //     if (playerOrDealer === 'player') {
-//     //         this.playerCards.push(nextCard);
-//     //     } else if (playerOrDealer === 'dealer') {
-//     //         this.dealerCards.push(nextCard);
-//     //     }
-
-//     //     if (show) {
-//     //         console.log(
-//     //             `${playerOrDealer.toUpperCase()} draws ${nextCard.value} of ${
-//     //                 nextCard.type
-//     //             }.`
-//     //         );
-//     //     } else {
-//     //         console.log(`${playerOrDealer.toUpperCase()} draws ???.`);
-//     //     }
-//     //     return nextCard;
-//     // }
-
-//     deal() {
-//         this.draw('player');
-//         this.draw('dealer');
-//         this.draw('player');
-//         this.draw('dealer', false); // Don't show the second dealer card.
-
-//         this.checkForBlackjack();
-
-//         this.dealerAmount = this.dealerLogic(true);
-//         this.playerAmount = this.playerLogic();
-//     }
-
-//     hit() {
-//         this.draw('player');
-//         this.playerAmount = this.playerLogic();
-//     }
-
-//     stay() {
-//         console.log(
-//             `~ DEALER shows ${this.dealerCards[1].value} of ${this.dealerCards[1].type}.`
-//         );
-//         this.dealerAmount = this.dealerLogic();
-//         this.checkResult();
-//     }
-
-//     checkResult() {
-//         if (this.playerAmount === this.dealerAmount) {
-//             console.log(
-//                 `Push! Dealer ${this.dealerAmount} - ${this.playerAmount} Player`
-//             );
-//             this.balance += this.currentBet;
-//         } else if (this.playerAmount > this.dealerAmount) {
-//             console.log(
-//                 `Player Wins! Player ${this.playerAmount} - ${this.dealerAmount} Dealer`
-//             );
-//             this.balance += this.currentBet * 2;
-//         } else {
-//             console.log(
-//                 `Dealer Wins! Dealer ${this.dealerAmount} - ${this.playerAmount} Player`
-//             );
-//         }
-//         this.updateBalance(this.balance);
-//     }
-
-//     checkForBlackjack() {
-//         if (this.playerCards[0].amount + this.playerCards[1].amount === 21) {
-//             //win
-//             console.log('User blackjack');
-//         } else if (
-//             this.dealerCards[0].amount + this.dealerCards[1].amount ===
-//             21
-//         ) {
-//             //lose
-//             console.log('Dealer blackjack');
-//         }
-//     }
-
-//     calculateTotal(playerOrDealer) {
-//         //playerOrDealer has to be either: this.playerCards or this.dealerCards
-//         let total = 0;
-//         let who = playerOrDealer;
-
-//         for (const card of who) {
-//             total += card.amount;
-//         }
-//         return total;
-//     }
-
-//     playerLogic() {
-//         let total = this.calculateTotal(this.playerCards);
-//         if (total > 21) {
-//             console.log(`* Bust! PLAYER has ${total}.`);
-//             //LOSE
-//         } else {
-//             console.log(`* PLAYER has ${total}.`);
-//             if (total === 21) {
-//                 //STAY FORCED
-//             }
-//         }
-//         return total;
-//     }
-
-//     dealerLogic(show = false) {
-//         let total = this.calculateTotal(this.dealerCards);
-//         if (show === true) {
-//             console.log(`~ DEALER has ${total}.`);
-//             return total;
-//         }
-//         while (total < 17) {
-//             console.log(`~ DEALER has ${total}.`);
-//             const nextCard = this.draw('dealer');
-//             total += nextCard.amount;
-//         }
-//         console.log(`~ DEALER has ${total}.`);
-//         return total;
-//     }
-// }
-
-// const game = new Game(1000);
